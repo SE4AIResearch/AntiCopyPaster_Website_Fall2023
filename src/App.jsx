c78aff7cf6d82751afb4ae4015ec6329e64973ce
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-10-02 16:17:17 -0400

    navigation bar and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Layout from './Layout';
+import ErrorPage from './ErrorPage';
+import Home from './Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage status={404} message={'Page not found.'} />
+      }
+    ]
+  }
+]);
 
 function App() {
-  return <p>Placeholder</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
