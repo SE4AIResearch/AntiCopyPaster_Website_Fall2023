7990997158bf5448832a1795fc8ca2be114ddd47
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-10-06 16:13:01 -0400

    About page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <p>Home</p>;
+  return (<>
+    <h1 className="center">AntiCopyPaster Plugin</h1>
+    <p className="subtitle center">
+      Fall 2023 &rarr; Spring 2024 Extensions
+    </p>
+    <h2>About</h2>
+    <p>
+      AntiCopyPaster is
+      an <a href="https://www.jetbrains.com/idea/">IntelliJ</a> plugin
+      with the goals of improving the development and refactoring experiences,
+      as well as reducing code redundancy. Upon detecting a pasted code
+      segment, it scans your active file for duplicates and suggests
+      refactoring if appropriate.
+    </p>
+    <h2>Our Improvements</h2>
+    <p>To be produced...</p>
+  </>);
 };
